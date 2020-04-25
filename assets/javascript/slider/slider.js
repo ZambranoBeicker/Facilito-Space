51fc92f8282b4219792588ae3435df48007995d5
beicker <beicker998@gmail.com>
2020-04-25 11:35:01 -0500

    Add slider.js and start slider logic

---
+++ assets/javascript/slider/slider.js
@@ -0,0 +1,25 @@
+export default class Slider{
+    constructor({elements,animationFunc}){
+        // debugger
+        this.index = 0
+        this.size = elements.length
+        console.log(this.size)
+    }
+    
+    next(){
+        this.index++
+        
+        if(this.index >= this.size) this.index = 0
+        console.log(this.index)
+        
+    }
+    
+    prev(){
+        this.index--
+        
+        if(this.index < 0) this.index = this.size -1
+        console.log(this.index)
+
+    }
+
+}
